64a19e9c15cbda09f40e1dc076f479e73e9c5e5f
jehd <jehd.jehzer@gmail.com>
2026-02-08 08:17:17 +0800

    ignore eslint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+type NextConfigWithEslint = NextConfig & {
+  eslint?: {
+    ignoreDuringBuilds?: boolean;
+  };
+};
+
+const nextConfig: NextConfigWithEslint = {
   /* config options here */
+  output: "export",
+  images: { unoptimized: true },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
